660ae3b29f754ff1cffbd847fb8e206027bdd05c
trungngotdt <trungngotdt@outlook.com.vn>
2017-10-03 20:41:49 +0700

    Revert "delete all"
    This reverts commit 476ca86a91b310f8f8e74a7b3078257cec1c1c2e.

---
+++ DiWithUnity/DiWithUnity/Services/IProductsService.cs
@@ -0,0 +1,15 @@
+﻿using DiWithUnity.M;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiWithUnity.Services
+{
+   public interface IProductsService
+    {
+        IEnumerable<Product> GetProducts();
+
+    }
+}
