3b9f86e77ddd678b9eb4f67c40058ee3fc32f0ec
trungngotdt <trungngotdt@outlook.com.vn>
2017-10-03 20:32:38 +0700

    1.0

---
+++ DiWithUnity/DiWithUnity/Services/ProductsService.cs
@@ -0,0 +1,22 @@
+﻿using DiWithUnity.M;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiWithUnity.Services
+{
+    public class ProductsService:IProductsService
+    {
+        public IEnumerable<Product> GetProducts()
+        {
+            return new List<Product>
+            {
+                new Product(){Name="Laptop",Price=1500 },
+                new Product(){Name="Xbox",Price=400},
+
+            };
+        }
+    }
+}
---
+++ DiWithUnity/DiWithUnity/V/ProductsPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using DiWithUnity.VM;
+using Microsoft.Practices.ServiceLocation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DiWithUnity.V
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProductsPage : ContentPage
+    {
+        public ProductsPage()
+        {
+            InitializeComponent();
+            BindingContext = ServiceLocator.Current.GetInstance<ProductsViewModel>();
+            
+        }
+    }
+}
